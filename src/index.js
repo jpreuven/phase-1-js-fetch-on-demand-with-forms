6328a4f208b94522832e7a621a4101d21fc5deac
Jonathan <jpreuven@gmail.com>
2023-06-18 03:04:14 -0700

    COMPLETED LAB

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
-  
-}
+  const form = document.querySelector("form");
+  const searchById = document.querySelector("#searchByID");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-document.addEventListener('DOMContentLoaded', init);
+    // These bottom two are the same thing
+    // console.log(e.target.children[1].value);
+    console.log(searchById.value);
+
+    fetch(`http://localhost:3000/movies/${searchById.value}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const title = document.querySelector("h4");
+        const summary = document.querySelector("p");
+
+        title.textContent = data.title;
+        summary.textContent = data.summary;
+      });
+
+    form.reset();
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
